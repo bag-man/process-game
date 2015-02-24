fb7b69bae458496bdbee352abf00fe21794e7500
Owen Garland <garland.owen@gmail.com>
2015-02-24 16:59:06 +0000

    COmmenting

--- quitme.c
+++ quitme.c
@@ -11,11 +11,13 @@
 #include <pthread.h>
 #include <sys/wait.h>
 #include <X11/Xutil.h>
+#include "quitme.h"
 
 #define TIME 1
 #define COUNTDOWN 10
 #define LIMIT 20
 
+/* Global vars, for shared memory and display / user checks */
 key_t key;
 int shmid;
 int *lost;
@@ -23,6 +25,74 @@ Window window;
 Display* display;
 int users;
 
+int main(int argc, char *argv[]) {
+
+  /* Define startup vars for shared memory */
+  key = 42;
+  shmid = shmget(key, 1, 0644 | IPC_CREAT);
+  lost = shmat(shmid, (void *)0, 0);
+  *lost = 0;
+
+  /* Define startup vars for display / user checks */
+  users = count_users();
+  display = XOpenDisplay(NULL);
+  window = has_focus();
+
+  /* Generate random process name */
+  srand(time(NULL));
+  int r = rand();
+  char pname[10];
+  sprintf(pname, "%d", r);
+  int size = strlen(argv[0]); 
+  strncpy(argv[0],pname,size); 
+  
+  /* Create fork */
+  pid_t cpid;
+  cpid = fork();
+
+  // Child fork
+  if(cpid == 0) { 
+    printf("Hit enter to keep the timer from reaching zero.\n");
+
+    /* Start threads for game and parent checker */
+    pthread_t parent, time, limiter;
+    pthread_create(&parent, NULL, check_parent, NULL);
+    pthread_create(&time, NULL, timer, NULL);
+    pthread_create(&limiter, NULL, limit, NULL);
+    signal(SIGTERM, end);
+    
+    /* If user hits enter, restart the countdown */
+    while(1) {
+      if(getchar()) {
+        pthread_cancel(time);
+        pthread_create(&time, NULL, timer, NULL);
+      }
+    }
+
+  // Parent 
+  } else {
+
+    /* Start threads for window and user checks */
+    pthread_t windowChecker, userChecker;
+    pthread_create(&windowChecker, NULL, check_window, NULL);
+    pthread_create(&userChecker, NULL, check_users, NULL);
+
+    /* Handle signals in parent window */
+    signal(SIGINT, end);
+    signal(SIGHUP, end);
+    signal(SIGTERM, end);
+    signal(SIGTSTP, end);
+    signal(SIGKILL, end);
+
+    /* Wait for child to die (Count down gets to 0) */
+    wait(0);
+    end();
+  }
+
+  return 0;
+}
+
+/* Function that is called when game is lost */
 void end() {
   if(*lost == 0) {
     printf("\n\nGame over.\n");
@@ -32,7 +102,7 @@ void end() {
   exit(0);
 }
 
-Window hasFocus() {
+Window has_focus() {
   Window w;
   int revert_to;
   XGetInputFocus(display, &w, &revert_to); 
@@ -64,7 +134,7 @@ void *check_users() {
 
 void *check_window() {
   while(1) {
-    if(hasFocus() != window) {
+    if(has_focus() != window) {
       end();
     }
   }
@@ -97,62 +167,3 @@ void *timer() {
   return 0;
 }
 
-int main(int argc, char *argv[]) {
-  pid_t cpid;
-  key = 42;
-  shmid = shmget(key, 1, 0644 | IPC_CREAT);
-  lost = shmat(shmid, (void *)0, 0);
-  *lost = 0;
-  users = count_users();
-
-  srand(time(NULL));
-  int r = rand();
-  char pname[10];
-  sprintf(pname, "%d", r);
-  int size = strlen(argv[0]); 
-  strncpy(argv[0],pname,size); 
-  
-  cpid = fork();
-
-  if(cpid == 0) { 
-
-    // Child
-    //printf("Parent pid %d\n", getppid());
-    printf("Hit enter to keep the timer from reaching zero.\n");
-
-    pthread_t pth, time, limiter;
-    pthread_create(&pth, NULL, check_parent, NULL);
-    pthread_create(&time, NULL, timer, NULL);
-    pthread_create(&limiter, NULL, limit, NULL);
-    signal(SIGTERM, end);
-    
-    while(1) {
-      if(getchar()) {
-        pthread_cancel(time);
-        pthread_create(&time, NULL, timer, NULL);
-      }
-    }
-
-  } else {
-
-    // Parent 
-    //printf("Child pid %d\n", cpid);
-
-    display = XOpenDisplay(NULL);
-    window = hasFocus();
-
-    pthread_t windowChecker, userChecker;
-    pthread_create(&windowChecker, NULL, check_window, NULL);
-    pthread_create(&userChecker, NULL, check_users, NULL);
-
-    signal(SIGINT, end);
-    signal(SIGHUP, end);
-    signal(SIGTERM, end);
-    signal(SIGTSTP, end);
-    signal(SIGKILL, end);
-    wait(0);
-    end();
-  }
-
-  return 0;
-}
---
+++ quitme.h
@@ -0,0 +1,8 @@
+void end();
+Window has_focus();
+int count_users();
+void *check_users();
+void *check_window();
+void *check_parent();
+void *limit();
+void *timer();
